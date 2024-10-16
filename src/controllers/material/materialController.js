ba77410e25c025847431def59e643463d9f6678b
Avazbek <qalandarovavazbek43@gmail.com>
2024-10-16 17:41:55 +0500

    added materials handlebarsd

--- src/controllers/material/materialController.js
+++ src/controllers/material/materialController.js
@@ -1,242 +1,212 @@
 const { validationResult, matchedData } = require("express-validator");
 const { Material } = require("../../models/materialModel");
 
-exports.createMaterial = async(req, res) => {
-    try {
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            const errorMessages = errors.array().map(error => error.msg);
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: errorMessages
-            });
-        }
-        const data = matchedData(req)
-
-        // Formatting date.
-        function formatDate(date) {
-            const d = new Date(date);
-            const hours = d.getHours();
-            const minutes = d.getMinutes();
-            const day = d.getDate();
-            const month = d.getMonth() + 1;
-            const year = String(d.getFullYear()).slice(-2);
-            return `${hours}:${String(minutes).padStart(2, '0')} ${day}.${month}.${year}`;
-        }
-        const now = new Date();
-        const formattedDate = formatDate(now);
-
-        const newMaterial = await Material.create({
-            name: data.name,
-            quantity: data.quantity,
-            createdAt: formattedDate
-        })
-
-        return res.status(200).send({
-            success: true,
-            error: false,
-            message: 'Material created successfully!',
-            data: newMaterial
-        })
-        
-    } catch (error) {
-        console.log(error);
-        if (error.message) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: error.message
-            })
-        }
-        return res.status(500).send({
-            success: false,
-            data: null,
-            error: "INTERVAL_SERVER_ERROR"
-        })
+exports.getCreateMaterial = async (req, res) => {
+  res.render("create-material", {
+    title: "Create material",
+    isCreateMaterial: true,
+  });
+};
+exports.createMaterial = async (req, res) => {
+  try {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: errorMessages,
+      });
     }
-}
+    const data = matchedData(req);
 
-exports.getAllMaterials = async(req, res) => {
-    try {
-        const material = await Material.find()
+    const newMaterial = await Material.create({
+      name: data.name,
+      quantity: data.quantity,
+      createdAt: new Date()
+    });
 
-        res.render('materials', {
-            title: 'Materials',
-            isMaterials: true,
-            material
-        })
-        
-
-        if (!material) {
-            return res.status(404).send({
-                success: false,
-                error: "Material not found!"
-            })
-        }
-    } catch (error) {
-        console.log(error);
-        if (error.message) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: error.message
-            })
-        }
-        return res.status(500).send({
-            success: false,
-            data: null,
-            error: "INTERVAL_SERVER_ERROR"
-        })
+    res.redirect('/api/materials')
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: error.message,
+      });
     }
-}
+    return res.status(500).send({
+      success: false,
+      data: null,
+      error: "INTERVAL_SERVER_ERROR",
+    });
+  }
+};
 
-exports.getOneMaterial = async(req, res) => {
-    try {
-        const id = req.params.id
+exports.getAllMaterials = async (req, res) => {
+  try {
+    const material = await Material.find();
 
-        // Checking id to valid.
-        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: "ID is not valid"
-            })
-        }
-    
-        const material = await Material.findById(id)
+    res.render("materials", {
+      title: "Materials",
+      isMaterials: true,
+      material,
+    });
 
-        return res.status(200).send({
-            success: true,
-            error: false,
-            material
-        })
-        
-    } catch (error) {
-        console.log(error);
-        if (error.message) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: error.message
-            })
-        }
-        return res.status(500).send({
-            success: false,
-            data: null,
-            error: "INTERVAL_SERVER_ERROR"
-        })
+    if (!material) {
+      return res.status(404).send({
+        success: false,
+        error: "Material not found!",
+      });
+    }
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: error.message,
+      });
+    }
+    return res.status(500).send({
+      success: false,
+      data: null,
+      error: "INTERVAL_SERVER_ERROR",
+    });
+  }
+};
+
+exports.getOneMaterial = async (req, res) => {
+  try {
+    const id = req.params.id;
+
+    // Checking id to valid.
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: "ID is not valid",
+      });
     }
 
+    const material = await Material.findById(id);
 
-}
+    res.render("material", {
+      title: "Material",
+      material,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: error.message,
+      });
+    }
+    return res.status(500).send({
+      success: false,
+      data: null,
+      error: "INTERVAL_SERVER_ERROR",
+    });
+  }
+};
+
+exports.getUpdateMaterial = async (req, res) => {
+  const id = req.params.id;
+  const material = await Material.findById(id);
+  res.render =
+    ("material",
+    {
+      material,
+    });
+};
 
 exports.updateMaterial = async (req, res) => {
-    try {
-        const id = req.params.id
+  try {
+    const id = req.params.id;
 
-        // Checking id to valid.
-        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: "ID is not valid"
-            })
-        }
-
-        const errors = validationResult(req)
-        if (!errors.isEmpty()) {
-            const errorMessages = errors.array().map(error => error.msg);
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: errorMessages
-            });
-        }
-        const data = matchedData(req)
-
-        const body = req.body
-        // Formatting date.
-        function formatDate(date) {
-            const d = new Date(date);
-            const hours = d.getHours();
-            const minutes = d.getMinutes();
-            const day = d.getDate();
-            const month = d.getMonth() + 1;
-            const year = String(d.getFullYear()).slice(-2);
-            return `${hours}:${String(minutes).padStart(2, '0')} ${day}.${month}.${year}`;
-        }
-        const now = new Date();
-        const formattedDate = formatDate(now);
-
-        const oldMaterial = await Material.findById(id)
-
-        const updateData = {
-            name: data.name || oldMaterial.name,
-            quantity: data.quantity || oldMaterial.quantity,
-            updatedAt: formattedDate
-        }
-
-        await Material.findByIdAndUpdate(id, updateData)
-
-        return res.status(200).send({
-            success: true,
-            error: false,
-            message: 'the material has been updated successfully!',
-            data: updateData
-        })
-
-    } catch (error) {
-        console.log(error);
-        if (error.message) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: error.message
-            })
-        }
-        return res.status(500).send({
-            success: false,
-            data: null,
-            error: "INTERVAL_SERVER_ERROR"
-        })
+    // Checking id to valid.
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: "ID is not valid",
+      });
     }
-}
+
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      const errorMessages = errors.array().map((error) => error.msg);
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: errorMessages,
+      });
+    }
+    const data = matchedData(req);
+
+    const oldMaterial = await Material.findById(id);
+
+    const updateData = {
+      name: data.name || oldMaterial.name,
+      quantity: data.quantity || oldMaterial.quantity,
+      updatedAt: new Date(),
+    };
+
+    await Material.findByIdAndUpdate(id, updateData);
+
+    res.redirect('/api/materials')
+
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: error.message,
+      });
+    }
+    return res.status(500).send({
+      success: false,
+      data: null,
+      error: "INTERVAL_SERVER_ERROR",
+    });
+  }
+};
 
 exports.deleteMaterial = async (req, res) => {
-    try {
-        const id = req.params.id
+  try {
+    const id = req.params.id;
 
-        // Checking id to valid.
-        if (!id.match(/^[0-9a-fA-F]{24}$/)) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: "ID is not valid"
-            })
-        }
+    res.redirect('/api/materials')
+    // Checking id to valid.
+    if (!id.match(/^[0-9a-fA-F]{24}$/)) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: "ID is not valid",
+      });
+    }
 
-        await Material.findByIdAndDelete(id)
+    await Material.findByIdAndDelete(id);
 
-        return res.status(200).send({
-            success: true,
-            error: false,
-            message: "Material deleted successfully"
-        })
+    res.redirect('/api/materials')
 
-    } catch (error) {
-        console.log(error);
-        if (error.message) {
-            return res.status(400).send({
-                success: false,
-                data: null,
-                error: error.message
-            })
-        }
-        return res.status(500).send({
-            success: false,
-            data: null,
-            error: "INTERVAL_SERVER_ERROR"
-        }) 
-    } 
-}
+  } catch (error) {
+    console.log(error);
+    if (error.message) {
+      return res.status(400).send({
+        success: false,
+        data: null,
+        error: error.message,
+      });
+    }
+    return res.status(500).send({
+      success: false,
+      data: null,
+      error: "INTERVAL_SERVER_ERROR",
+    });
+  }
+};
--- src/routes/materialRouter/materialRouter.js
+++ src/routes/materialRouter/materialRouter.js
@@ -1,12 +1,14 @@
 const { checkSchema } = require('express-validator')
-const { getAllMaterials, createMaterial, updateMaterial, deleteMaterial, getOneMaterial } = require('../../controllers/material/materialController')
+const { getAllMaterials, createMaterial, updateMaterial, deleteMaterial, getOneMaterial, getUpdateMaterial, getCreateMaterial } = require('../../controllers/material/materialController')
 const { materialSchema, updateMaterialSchema } = require('../../util/validators/materialValidate')
 
 const router = require('express').Router()
 
 router
+.get('/create-material', getCreateMaterial)
 .get('/materials', getAllMaterials)
 .get('/material/:id', getOneMaterial)
+.get('/update-material/:id', getUpdateMaterial)
 .post('/create-material', checkSchema(materialSchema), createMaterial)
 .post('/update-material/:id', checkSchema(updateMaterialSchema), updateMaterial)
 .post('/delete-material/:id', deleteMaterial)
