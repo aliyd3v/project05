065f6bf0b91f2bc8614196032f4c405887142879
aliyd3v <nabiy5511@gmail.com>
2024-10-23 11:42:39 +0500

    I don't know what I change

--- src/routes/materialRouter/materialRouter.js
+++ src/routes/materialRouter/materialRouter.js
@@ -1,17 +1,19 @@
 const { checkSchema } = require('express-validator')
-const { getAllMaterials, createMaterial, updateMaterial, deleteMaterial, getOneMaterial, getUpdateMaterial, getCreateMaterial } = require('../../controllers/material/materialController')
+const { getAllMaterials, createMaterial, updateMaterial, deleteMaterial, getOneMaterial, getUpdateMaterial, getCreateMaterial, deleteAllMaterials, getDdelteMaterial } = require('../../controllers/material/materialController')
 const { materialSchema, updateMaterialSchema } = require('../../util/validators/materialValidate')
 const { roleAccessMiddleware } = require('../../middlewares/role-access-middleware')
 
 const router = require('express').Router()
 
 router
-.get('/create-material', roleAccessMiddleware(['admin']), getCreateMaterial)
-.get('/materials', roleAccessMiddleware(['admin']), getAllMaterials)
-.get('/material/:id', roleAccessMiddleware(['admin']), getOneMaterial)
-.post('/create-material', roleAccessMiddleware(['admin']), checkSchema(materialSchema), createMaterial)
-.get('/material/:id/update', roleAccessMiddleware(['admin']), getUpdateMaterial)
-.post('/material/:id/update', roleAccessMiddleware(['admin']), checkSchema(updateMaterialSchema), updateMaterial)
-.post('/delete-material/:id', roleAccessMiddleware(['admin']), deleteMaterial)
+    .get('/create-material', roleAccessMiddleware(['admin']), getCreateMaterial)
+    .get('/materials', roleAccessMiddleware(['admin']), getAllMaterials)
+    .get('/material/:id', roleAccessMiddleware(['admin']), getOneMaterial)
+    .post('/create-material', roleAccessMiddleware(['admin']), checkSchema(materialSchema), createMaterial)
+    .get('/material/:id/update', roleAccessMiddleware(['admin']), getUpdateMaterial)
+    .post('/material/:id/update', roleAccessMiddleware(['admin']), checkSchema(updateMaterialSchema), updateMaterial)
+    .get('/material/:id/delete', roleAccessMiddleware(['admin']), getDdelteMaterial)
+    .post('/material/:id/delete', roleAccessMiddleware(['admin']), deleteMaterial)
+    .post('/materials/delete', roleAccessMiddleware(['admin']), deleteAllMaterials)
 
 module.exports = router
