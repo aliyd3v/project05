4508a4cfb4e247ec78c4315445d769100921daa7
aliyd3v <nabiy5511@gmail.com>
2024-10-23 12:33:36 +0500

    changed materialValidate.js

--- src/util/validators/materialValidate.js
+++ src/util/validators/materialValidate.js
@@ -21,11 +21,17 @@ exports.updateMaterialSchema = {
     name: {
         isString: {
             errorMessage: "Material nomi string bo'lishi kerak"
+        },
+        notEmpty: {
+            errorMessage: "Material nomi talab qilinadi"
         }
     },
     quantity: {
         isInt: {
             errorMessage: "Material hajmi raqam bo'lishi kerak"
+        },
+        notEmpty: {
+            errorMessage: "Material hajmi talab qilinadi"
         }
     }
 }
