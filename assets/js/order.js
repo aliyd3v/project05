a11c45bbe63f67abb5a6cec70b8daf5bc9399b91
ali <nabiy5511@gmail.com>
2025-03-03 16:59:28 +0500

    cart added

---
+++ assets/js/order.js
@@ -0,0 +1,12 @@
+(function () {
+    "use strict";
+
+    let cartContainer = document.getElementById('cart-container')
+    let emptyMessage = document.getElementById('empty-message')
+    let containerOrdering = document.getElementById('container-ordering')
+    let orderButton = document.getElementById('order-button')
+
+    containerOrdering.style.display = 'none'
+    orderButton.style.display = 'none'
+
+})()
